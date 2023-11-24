55c0f4c2a301cce5761bb86c2a0f4ae9908766bd
Pramod Ray <pramod0215@gmail.com>
2023-11-24 11:22:26 +0530

    handlebars engine add

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
+const path = require('path');
+
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended:false}));
-const adminRoutes = require('./routes/admin');
+const expressHbs = require('express-handlebars');
+
+const app = express();
+
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+  })
+);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-app.use(adminRoutes);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
-app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found!</h1>');
-})
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
+
 app.listen(3000);
