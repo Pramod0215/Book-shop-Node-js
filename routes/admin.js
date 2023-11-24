e1c91a6abd2b11a5f20bb3142f6a6f18e0eee32b
Pramod Ray <pramod0215@gmail.com>
2023-11-24 15:50:21 +0530

    add function

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,18 @@
+const path = require('path');
+
 const express = require('express');
+
+const adminController = require('../controllers/admin');
+
 const router = express.Router();
 
-router.get('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="post"><input name="title" type="text"/><input type="submit" value="add Product"/></form>')
-});
-router.post('/product',(req,res,next)=>{
-    res.redirect('/')
-});
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,19 @@
+const path = require('path');
+
 const express = require('express');
+
+const shopController = require('../controllers/shop');
+
 const router = express.Router();
 
-router.get('/',(req,res,next)=>{
-    res.send('<h1> Welocome express js</h1>')
-});
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
