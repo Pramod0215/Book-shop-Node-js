829fa4b3c0b0405984597e26a33a7b86a9d7a842
Pramod Ray <pramod0215@gmail.com>
2023-11-24 09:35:11 +0530

    Add Pug

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
+const path = require('path');
+
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended:false}));
-const adminRoutes = require('./routes/admin');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-app.use(adminRoutes);
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
-app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found!</h1>');
-})
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
+
 app.listen(3000);
